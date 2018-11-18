dd4db1077b9eaab32372dbc9672c6f8bbc79f3f6
pkhilenko <pkhilenko@yandex.ru>
2018-11-18 19:14:48 +0600

    git step-3

---
+++ app/javascript/packs/components/Person-details/Person-details.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import './Person-details.scss';
+import SwapiService from '../../../services/swapi-service'
+import Spinner from '../Spinner/Spinner'
+import ErrorIndicator from '../Error-indicator/Error-indicator'
+
+export default class PersonDetails extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    person: null
+  };
+
+  componentDidMount() {
+    this.updatePerson();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.personId !== prevProps.personId) {
+      this.updatePerson();
+    }
+  }
+
+  updatePerson() {
+    const { personId } = this.props;
+    if (!personId) {
+      return;
+    }
+
+    this.swapiService
+      .getPerson(personId)
+      .then((person) => {
+        this.setState({ person });
+      });
+  }
+  render() {
+    if (!this.state.person) {
+      return (
+        <div>
+          <span>Select a person from a list</span>;
+          <Spinner />
+        </div>
+      
+      )
+    }
+
+    const { id, name, gender,
+      birthDay, eyeColor } = this.state.person;
+
+    return (
+      <div className="Person-details card">
+        <img className="person-image"
+          src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          alt="character" />
+
+        <div className="card-body">
+          <h4>{name} {this.props.personId}</h4>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span className="term">Gender</span>
+              <span>{gender}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Birth Day</span>
+              <span>{birthDay}</span>
+            </li>
+            <li className="list-group-item">
+              <span className="term">Eye Color</span>
+              <span>{eyeColor}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ app/javascript/packs/components/Random-planet/Random-planet.jsx
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import './Random-planet.scss';
+import SwapiService from '../../../services/swapi-service'
+import Spinner from '../Spinner/Spinner'
+import ErrorIndicator from '../Error-indicator/Error-indicator'
+
+
+
+export default class RandomPlanet extends Component {
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+    loading: true,
+    error: false
+  };
+
+  componentDidMount() {
+    this.updatePlanet()
+    this.interval = setInterval(() => { 
+      this.updatePlanet() 
+    }, 10000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+
+  onPlanetLoaded = (planet) => {
+    this.setState({ planet, loading: false });
+  };
+
+  onError = (err) => {
+    console.log(err, 'ERR')
+    this.setState({
+      error:true,
+      loading: false
+    })
+  };
+
+  updatePlanet = () => {
+    const id = Math.floor(Math.random()*25 +1);
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
+  }
+
+  render() {
+    const { planet, loading, error } = this.state;
+    const hasData = !(loading || error)
+    const errorMessage = error ? <ErrorIndicator /> : null
+    const spinner = loading ? <Spinner /> :  null
+    const content = hasData ? <PlanetView planets={planet} /> : null
+
+    return (
+      <div className="Random-planet jumbotron rounded">
+          {spinner}
+          {content}
+          {errorMessage}
+      </div>
+
+    );
+  }
+}
+
+const PlanetView = ({ planets }) => {
+  const { id, name, population, rotationPeriod, diameter} = planets
+  return (
+    <React.Fragment>
+      <img className="planet-image"
+        src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+      <div>
+        <h4>{name}</h4>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <span className="term">Population: </span>
+            <span>{population}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Rotation Period: </span>
+            <span>{rotationPeriod}</span>
+          </li>
+          <li className="list-group-item">
+            <span className="term">Diameter: </span>
+            <span>{diameter}</span>
+          </li>
+        </ul>
+      </div>
+    </React.Fragment>
+  )
+}
