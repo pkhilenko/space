00c7932ad19e871a875760911400ec08a246a63a
pkhilenko <pkhilenko@yandex.ru>
2018-11-20 18:18:34 +0600

    step5-rf

--- app/javascript/packs/components/Item-list/Item-list.jsx
+++ app/javascript/packs/components/Item-list/Item-list.jsx
@@ -7,37 +7,41 @@ export default class ItemList extends Component {
   swapiService = new SwapiService();
 
   state = {
-    peopleList: null
+    itemList: null
   };
 
   componentDidMount() {
-    this.swapiService
-      .getAllPeople()
-      .then((peopleList) => {
+    const { getData } = this.props
+
+     getData()
+      .then((itemList) => {
         this.setState({
-          peopleList
+          itemList
         });
       });
   }
 
   renderItems(arr) {
-    return arr.map(({ id, name }) => {
+    return arr.map((item) => {
+      const { id } = item
+      const label = this.props.renderItem(item)
+
       return (
         <li className="list-group-item"
           key={id}
           onClick={() => this.props.onItemSelected(id)}>
-          {name}
+          {label}
         </li>
       );
     });
   }
   render() {
-    const { peopleList } = this.state;
+    const { itemList } = this.state;
 
-    if (!peopleList) {
+    if (!itemList) {
       return <Spinner />;
     }
-    const items = this.renderItems(peopleList);
+    const items = this.renderItems(itemList);
     return (
       <ul className="Item-list list-group">
           {items}
